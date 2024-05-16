7e738695bb7ea038e274f82fc589316ccd022bc2
Candace Ryan <ryan.candace26@gmail.com>
2024-05-16 14:36:08 -0500

    added routes

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -7,9 +7,16 @@ const Navbar = () => {
         <NavLink to="/" className="w-10 h-10 rounded-lg bg-white items-center justify-center flex font-bold shadow-md">
             <p className="pink-gradient_text">CR</p>
         </NavLink>
-        <nav>
-            
-        </nav>
+    <nav className="flex text-lg gap-7 font-medium">
+        <NavLink to="/about" className={({ isActive }) => isActive?
+            'text-pink-500' : 'text-black'}>
+                About
+            </NavLink>
+        <NavLink to="/projects" className={({ isActive }) => isActive?
+            'text-pink-500' : 'text-black'}>
+                Projects
+            </NavLink>
+     </nav>
     </header>
   )
 }
