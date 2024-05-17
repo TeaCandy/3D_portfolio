5f554369d8a56819543c17f42da243edcabd0534
Candace Ryan <ryan.candace26@gmail.com>
2024-05-16 23:58:20 -0500

    added 3d poly models

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,34 @@
-import React from 'react'
+import {Suspense} from 'react'
+import { Canvas } from '@react-three/fiber'
+import Loader from '../components/Loader'
 
+import Island from '../models/Island';
+
+{/* <div className="absolute top-28 left-0 right-0 z-10 flex items-center justify-center">
+    POPUP
+</div> */}
 const Home = () => {
-  return (
-    <section className='w-full'>
+    const adjustIslandForScreenSize = () => {
+        let screenScale, 
+    }
 
+
+  return (
+    <section className="w-full h-screen relative">
+        <Canvas 
+        className="w-full h-screen bg-trasnparent"
+        camera={{ near: 0.1, far: 1000  }}
+        >
+           <Suspense fallback={<Loader />}>
+            <directionalLight />
+            <ambientLight />
+            <pointLight />
+            <spotLight />
+            <hemisphereLight />
+
+                <Island />
+            </Suspense> 
+        </Canvas>
     </section>
   )
 }
