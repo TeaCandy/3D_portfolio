dc227f2801c98dba1c5269da6171475cd1f5dc1f
Candace Ryan <ryan.candace26@gmail.com>
2024-05-22 23:34:59 -0500

    added .env with keys to emailjs for public form

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,12 +1,45 @@
-import React, { useState } from 'react'
+import React, { useRef, useState } from 'react'
+import emailjs from '@emailjs/browser';
 
 const Contact = () => {
+  const formRef = useRef(null);
   const [form, setForm] = useState({ name: '', email: '', message: ''})
   const [isLoading, setIsLoading] = useState(false);
 
-  const handleChange = () => {};
-  const handleFocus = () => {};
+  const handleChange = (e) => {
+    setForm({...form, [e.target.name]: e.target.value})
+  };
+  
+  const handleFocus = () => {
+  };
   const handleBlur = () => {};
+  
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setIsLoading(true);
+
+    emailjs.send(
+     import.meta.env.VITE_APP_EMAILJS_SERVICE_ID,
+     import.meta.env.VITE_APP_EMAILJS_TEMPLATE_ID,
+     {
+      from_name: form.name,
+      to_name: "Candace",
+      from_email: form.email,
+      to_email: 'ryan.candace26@gmail.com',
+      message: form.message
+     },
+     import.meta.env.VITE_APP_EMAILJS_PUBLIC_KEY
+    ).then(() => {
+      setIsLoading(false);
+      //TODO: send success alert, hide alert
+
+      setForm({name: '', email: '', message: ''});
+    }).catch((error) => {
+      setIsLoading(false);
+      console.log(error);
+      // error message if there is one
+    })
+  };
 
   return (
     <section className="relative flex lg:flex-row flex-col max-container">
@@ -16,6 +49,7 @@ const Contact = () => {
         </h1>
         <form
           className="w-full flex flex-col gap-7 mt-14"
+          onSubmit={handleSubmit}
         >
           <label className="text-black-500 font-semibold">
             Name
