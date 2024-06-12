449f6dd3da7ca3d1009318039cfa72e15e3d014d
Candace Ryan <ryan.candace26@gmail.com>
2024-06-12 14:24:12 -0500

    test for env variables

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,14 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig, loadEnv } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
+// Load environment variables from .env file
+const env = loadEnv(process.env.MODE, process.cwd());
+
 export default defineConfig({
   plugins: [react()],
-  assetsInclude: ['**/*.glb']
-})
+  assetsInclude: ['**/*.glb'],
+  define: {
+    // Make environment variables available to the client-side code
+    'process.env': Object.assign({}, env)
+  },
+});
